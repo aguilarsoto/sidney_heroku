1ba3d617deba867ba8b79a3d539b1f93ae1c103e
Marco Vinicio Aguilar Soto <aguilarsoto@gmail.com>
2011-09-29 09:32:59 -0600

    start sidney heroku, first commit, may the force be with you and what are midi-chlorians?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SidneyHeroku::Application.config.secret_token = 'ae50c7a9c3189f41b410bbd0b0c5bb9dd455a3a0672a024e1aa792d9fe0404f8a68c87220fbe037c7d9e138551e36a022fb1df7f7f643ecbd90a784e3e759be6'
