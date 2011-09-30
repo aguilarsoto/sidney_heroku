937e73b03ceeb4fa63913c85d75999d42f745755
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2011-09-30 15:24:49 -0700

    and coma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-   searchable do.
+   searchable do
       text :first_name, :last_name, :default_boost => 2
       text :keywords, :multi => true do
         [first_name, last_name, address.state]
