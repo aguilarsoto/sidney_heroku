85156994c84a51c018c94c40e021c5a726a80f4c
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2011-09-30 15:28:57 -0700

    remove multi call and retest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
    searchable do
       text :first_name, :last_name, :default_boost => 2
-      text :keywords, :multi => true do
+      text :keywords do
         [first_name, last_name, address.state]
       end
       text :state do
