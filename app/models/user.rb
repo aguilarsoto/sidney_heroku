eb950835bc6f3479c42d51cebb93e56b0db5b39b
Marco V. Aguilar Soto <aguilarsoto@gmail.com>
2011-09-30 15:18:19 -0700

    add solr support.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+   searchable do.
+      text :first_name, :last_name, :default_boost => 2
+      text :keywords, :multi -> true do
+        [first_name, last_name, address.state]
+      end
+      text :state do
+         address.state
+      end
+   end
+
 end
